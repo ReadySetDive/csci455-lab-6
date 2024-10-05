578283b6464967a18b0d7e11aeb350802f5eb328
Matthew Gordy <matt.grdy@gmail.com>
2024-10-05 11:54:45 -0700

    restructuring -> more functionalish

--- .gc.sh
+++ .gc.sh
@@ -1,6 +1,8 @@
 ssh_cfg_dir="../../../.ssh/config"
 repo_owner="ReadySetDive"
 repo_name="csci455-lab-6"
+name="Matthew Gordy"
+email="mgordy@usc.edu"
 
 repo_http_address="https://github.com/$repo_owner/$repo_name.git"
 repo_ssh_address="git@github.com:$repo_owner/$repo_name.git"
@@ -8,8 +10,7 @@ repo_ssh_address="git@github.com:$repo_owner/$repo_name.git"
 printf "HTTP REPO ADDRESS: https://github.com/$repo_owner/$repo_name.git\n"
 printf " SSH REPO ADDRESS: git@github.com:$repo_owner/$repo_name.git\n\n"
 
-chmod 600 $ssh_cfg_dir
-chmod 700 $ssh_cfg_dir
+
 
 add_ssh_key() {
     KEY_FILE="$PWD/$1"   # Get the full path to the key file in the current directory
@@ -36,32 +37,45 @@ add_ssh_key() {
     fi
 }
 
+fix_ssh_cfg () {
+    chmod 600 $ssh_cfg_dir
+    chmod 700 $ssh_cfg_dir
+}
+
+clean_all () {
+rm -rf .git*
+rm -rf .auth*
+}
+
+config_local_git () {
+    git init
+    git remote add origin $repo_ssh_address
+    git config user.name $name
+    git config user.email $email
+    git add .
+    git commit -m "initial commit"
+    git branch -m master main
+    touch .gitignore
+    echo ".voc/*" >> .gitignore
+    echo "*auth*" >> .gitignore
+}
+
+ssh_setup () {
+    mkdir .auth
+    ssh-keygen -t ed25519 -C "deploy-key" -f ./.auth/deploy_key -N ""
+    touch $ssh_cfg_dir
+    add_ssh_key .auth/deploy_key
+}
+
 initp () {
    git push -u origin main
 }
 
-rm -rf .git*
-rm -rf .auth*
-git init
-git remote add origin $repo_ssh_address
+fix_ssh_cfg
+clean_all
+config_local_git
+ssh_setup
 
-
-
-git config user.name "Matthew Gordy"
-git config user.email "mgordy@usc.edu"
-git add .
-git commit -m "initial commit"
-git branch -m master main
-touch .gitignore
-echo ".voc/*" >> .gitignore
-echo "*auth*" >> .gitignore
-
-
-# authentication steps here
-mkdir .auth
-ssh-keygen -t ed25519 -C "deploy-key" -f ./.auth/deploy_key -N ""
-touch $ssh_cfg_dir
-add_ssh_key .auth/deploy_key
 
 printf"\n\n"
 printf "MAKE KEY AT THE FOLLOWING:    https://github.com/$repo_owner/$repo_name/settings/keys/new\n"
